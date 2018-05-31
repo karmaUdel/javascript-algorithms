7439046ee2572586a73af4a4b718043ac6b9463f
karmaUdel <karma@udel.edu>
2018-05-31 16:32:27 -0400

    adding remove to AVL

--- src/data-structures/tree/avl-tree/AvlTree.js
+++ src/data-structures/tree/avl-tree/AvlTree.js
@@ -137,4 +137,27 @@ export default class AvlTree extends BinarySearchTree {
     // Attach rootNode to the left of rightNode.
     rightNode.setLeft(rootNode);
   }
+  
+  remove(node){
+  /**
+  * Let node be the node to be deleted
+  * 1) Perform standard BST delete for node.
+  * 2) Starting from node, travel up and find the first unbalanced node. Let z be the first unbalanced node, y be the larger height child of z, and x be the larger height child of y. Note that the definitions of x and y are different from insertion here.
+  * 3) Re-balance the tree by performing appropriate rotations on the subtree rooted with z. There can be 4 possible cases that needs to be handled as x, y and z can be arranged in 4 ways. Following are the possible 4 arrangements:
+	  * a) y is left child of z and x is left child of y (Left Left Case)
+	  * b) y is left child of z and x is right child of y (Left Right Case)
+	  * c) y is right child of z and x is right child of y (Right Right Case)
+	  * d) y is right child of z and x is left child of y (Right Left Case)
+  */
+	// remove the node treating the node as BST node
+	super.remove(node);
+	// get parent of deleted node
+	let currentNode = this.root.find(node.parent.value);
+    // start balancing
+	while (currentNode) {
+      this.balance(currentNode);
+      currentNode = currentNode.parent;
+    }
+
+  }
 }
